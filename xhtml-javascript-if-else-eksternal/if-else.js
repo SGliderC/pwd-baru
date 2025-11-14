8ad3639ca66d12d9b833c286a2c8721d3852f4e2
user <salvatoreandreaaudrich@gmail.com>
2025-11-14 15:57:22 +0700

    p

---
+++ xhtml-javascript-if-else-eksternal/if-else.js
@@ -0,0 +1,16 @@
+var iAge = prompt("Please enter your age in years.", 0);
+var sMF = prompt("Are you a (M)ale or (F)emale?", "M");
+
+if (iAge > 20) {
+  if (sMF === "M") {
+    alert("You are a male over 20 years of age");
+  } else {
+    alert("You are a female over 20 years of age");
+  }
+} else {
+  if (sMF === "M") {
+    alert("You are a male under the age of 21");
+  } else {
+    alert("You are a female under the age of 21");
+  }
+}
